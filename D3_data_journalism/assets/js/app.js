d0654082df0622879c69f6325471bf10fcd0d82a
unknown <nguye453@cougars.csusm.edu>
2021-05-25 21:28:03 -0700

    beginning of app.js - intitialization of svg

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,27 @@
-// @TODO: YOUR CODE HERE!
+// SVG wrapper dimensions
+var svgWidth = 1200;
+var svgHeight = 660;
+
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+// append svg and group
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "smokes";
+var chosenYAxis = "age";
+
