599d3737bdd6936ce93cbcba30a5374b6e725ee6
unknown <nguye453@cougars.csusm.edu>
2021-05-31 01:21:35 -0700

    finished chart and data points

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,6 @@
 // SVG wrapper dimensions
-var svgWidth = 1200;
-var svgHeight = 660;
+var svgWidth = 800;
+var svgHeight = 600;
 
 var margin = {
   top: 50,
@@ -21,7 +21,62 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params
+// initial Params
 var chosenXAxis = "smokes";
 var chosenYAxis = "age";
 
+// main function call
+d3.csv("assets/data/data.csv").then(plotData);
+
+// main function with csv passed in
+function plotData(importData) {
+  importData.map(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+  });
+
+  // two functions to update scaling
+  var xScale = d3.scaleLinear()
+    .domain([8, d3.max(importData, d => d.smokes)])
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([30, d3.max(importData, d => d.age)])
+    .range([height, 0]);
+
+  // render axes
+  var bottomAxis = d3.axisBottom(xScale);
+  var leftAxis = d3.axisLeft(yScale);
+
+  // append axes to chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // render data points
+  var dataPoints = chartGroup.selectAll("circle")
+    .data(importData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.smokes))
+    .attr("cy", d => yScale(d.age))
+    .attr("r", "13")
+    .attr("fill", "#1ba6c2")
+    .attr("opacity", ".8")
+
+  // append to graph
+  var dataPoints = chartGroup.selectAll()
+    .data(importData)
+    .enter()
+    .append("text")
+    .attr("x", d => xScale(d.smokes))
+    .attr("y", d => yScale(d.age))
+    .style("font-size", "13px")
+    .style("text-anchor", "middle")
+    .style('fill', 'white')
+    .text(d => (d.abbr));
+
+  // add tooltip info  
+}
