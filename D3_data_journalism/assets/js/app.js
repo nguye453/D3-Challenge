a69d044956330c18e76f92725d19e1f9fefea7f4
unknown <nguye453@cougars.csusm.edu>
2021-05-31 23:48:43 -0700

    finished assignment - small visual fixes to come

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -29,8 +29,10 @@ var chosenYAxis = "age";
 d3.csv("assets/data/data.csv").then(plotData);
 
 // main function with csv passed in
-function plotData(importData) {
-  importData.map(function(data) {
+function plotData(importData) 
+{
+  importData.map(function(data) 
+  {
     data.age = +data.age;
     data.smokes = +data.smokes;
   });
@@ -62,7 +64,7 @@ function plotData(importData) {
     .append("circle")
     .attr("cx", d => xScale(d.smokes))
     .attr("cy", d => yScale(d.age))
-    .attr("r", "13")
+    .attr("r", "15")
     .attr("fill", "#1ba6c2")
     .attr("opacity", ".8")
 
@@ -73,10 +75,43 @@ function plotData(importData) {
     .append("text")
     .attr("x", d => xScale(d.smokes))
     .attr("y", d => yScale(d.age))
-    .style("font-size", "13px")
+    .style("font-size", "15px")
     .style("text-anchor", "middle")
     .style('fill', 'white')
     .text(d => (d.abbr));
 
-  // add tooltip info  
+  // add tooltip info
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([70, -50])
+    .html(function (d) 
+    {
+      return (`${d.state}<br>Median Age: ${d.age}<br>Smokers: ${d.smokes}% `);
+    });
+  chartGroup.call(toolTip);
+  
+  // mouseover
+  dataPoints.on("mouseover", function(data) 
+  {
+    toolTip.show(data, this);
+  })
+  // mouseout
+  .on("mouseout", function(data) 
+  {
+    toolTip.hide(data, this);
+  });
+
+  //add axis labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Age");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top - 10})`)
+    .attr("class", "axisText")
+    .text("Smokers (%)");
 }
